5992f46b22d17ae89e7243f0f57992ee04c91ad3
Aisha Bakari <144688546+Aishabakari@users.noreply.github.com>
2025-09-25 16:44:35 -0400

    Update PerfectSquare.java

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -1,19 +1,21 @@
-public class PerfectSquare {
-	
-	public static void main(String[] args) {
-		System.out.println(isPerfectSquare(1));
-		System.out.println(isPerfectSquare(4));
-		System.out.println(isPerfectSquare(Integer.MAX_VALUE/100));
-		System.out.println(isPerfectSquare(255));
-		
-	}
-	
-	public static boolean isPerfectSquare(int num) {
-        for(int i = 1; i < num; i++) {
-        	if(i*i = num) 
-        		return true;
-        	else if (i*i > num) return false;
-        }
-    }
+package com.bptn.course._01_variables;
 
+public class PerfectSquare {
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(1));
+        System.out.println(isPerfectSquare(4));
+        System.out.println(isPerfectSquare(Integer.MAX_VALUE/100));
+        System.out.println(isPerfectSquare(255));
+    }
+    
+    public static boolean isPerfectSquare(int num) {
+        for (int i = 1; i * i <= num; i++) {
+            if (i * i == num) { // Check if i squared equals num
+                return true; // A perfect square is found
+            } else if (i * i > num) {
+                return false; // i squared exceeds num, so it's not a perfect square
+            }
+        }
+        return false; // Return false if no perfect square is found
+    }
 }
